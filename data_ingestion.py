fd8646b936c861c1f85abf371c5547190a8a63a4
erenyilmaz <eren_yilmaz_2010@hotmail.com>
2025-12-30 00:43:31 +0300

    Cloud DB tasima yapildi

--- data_ingestion.py
+++ data_ingestion.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 # YENİ ADRES BURASI:
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_pinecone import PineconeVectorStore
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
